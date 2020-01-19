478d1f990afcab3a057cc755008c5e60195b2913
neb <10528790@my.uvu.edu>
2020-01-19 14:02:00 -0700

    added event model and limited results via http url

---
+++ models/event.js
@@ -0,0 +1,11 @@
+class Event {
+    constructor(id, summary, description, start, end) {
+        this.id = id;
+        this.summary = summary;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+    }
+}
+
+export default Event;
--- store/actions/calendar.js
+++ store/actions/calendar.js
@@ -1,14 +1,19 @@
+import Event from '../../models/event';
+
 export const GET_EVENTS = 'GET_EVENTS';
 
 export const getEvents = () => {
     return async dispatch => {
         const CALENDAR_ID = 'jn.web.developer%40gmail.com';
         const API_KEY = 'AIzaSyDg7_XJNVaiMIOkgSqZfZ6ivpBhnyv6UIQ '
-        let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`;
+        let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?maxResults=1&key=${API_KEY}`;
         const res = await fetch(url);
         const resData = await res.json();
         console.log(resData);
-        const setEvents = resData;
+        const setEvents = [];
+        for(const key in resData) {
+            setEvents.push(new Event(key, resData[key].summary, resData[key].description, resData[key].start, resData[key].end));
+        }
         dispatch({type: GET_EVENTS, events: setEvents});
     }
 }
