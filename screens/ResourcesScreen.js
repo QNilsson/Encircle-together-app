06881c1fe76617b68f86e3df2424b5e1f4a6b92c
neb <10528790@my.uvu.edu>
2020-01-31 22:55:40 -0700

    added publication model, added flatlist to display issuu api data

--- screens/ResourcesScreen.js
+++ screens/ResourcesScreen.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import Publication from '../models/publication';
 
 export default class ResourcesScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: {}
-    }
+      items: []
+    };
   }
 
   componentDidMount() {
@@ -19,13 +21,37 @@ export default class ResourcesScreen extends Component {
     
     fetch(url)
     .then((res) => res.json())
-    .then((resData) => console.log(resData));
+    .then((resData) => {
+      const data = [];
+      for(const key in resData.rsp._content.result._content) {
+        data.push(
+          new Publication(
+            resData.rsp._content.result._content[key].document.documentId, 
+            resData.rsp._content.result._content[key].document.publicationId, 
+            resData.rsp._content.result._content[key].document.title, 
+            resData.rsp._content.result._content[key].document.description, 
+            resData.rsp._content.result._content[key].document.publishDate)
+        );
+      }
+      for(const i in data) {
+        console.log(data[i])
+      }
+      this.setState({
+        items: data
+      });
+    })
+    .catch((err) => console.log(err));
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Resources Screen</Text>
+        <FlatList
+        data={this.state.items}
+        keyExtractor={item => item.docId}
+        renderItem={({ item }) => <View key={item.docId}><Text>{item.title}</Text></View>}
+      />
       </View>
     );
   }
