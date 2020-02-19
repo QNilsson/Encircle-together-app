dd262af856042fe8d003e9bf9f95d1b7f141fdc1
Aaron Price <aaronprice@19142-APrice.local>
2020-02-19 12:56:33 -0700

    Added iframe of encirclelive. Still needs love.

--- screens/EncircleLiveScreen.js
+++ screens/EncircleLiveScreen.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 const EncircleLiveScreen = props => {
   return (
-    <View style={styles.container}>
-      <Text>Encircle Live Screen</Text>
-      <Text>Test</Text>
-    </View>
+    <WebView
+      source={{
+        html:
+          '<iframe width="100%" height="100%" src="https://www.encirclelive.org" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+      }}
+      style={{ marginTop: 30 }}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default EncircleLiveScreen;
