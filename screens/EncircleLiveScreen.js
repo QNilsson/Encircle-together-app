eabf2a49e8a692fbbf894bc50ec40bc3c2284c66
Aaron Price <aaronprice@19142-APrice.local>
2020-02-19 13:00:55 -0700

    fixed import mistake

--- screens/EncircleLiveScreen.js
+++ screens/EncircleLiveScreen.js
@@ -1,6 +1,3 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
