5634e05f35c59084936d25423ec352b5d99fbf7f
neb <10528790@my.uvu.edu>
2020-03-17 21:33:11 -0600

    resource name switch statement setup

--- screens/ResourceScreen.js
+++ screens/ResourceScreen.js
@@ -5,21 +5,59 @@ import { WebView } from 'react-native-webview';
 
 const ResourceScreen = (props) => {
   const name = props.navigation.getParam('resourceName');
-  // let url = '';
-  // switch(name) {
-  //   case 'gsatoolkit':
-  //     url = '';
-  //     break;
-  //   default:
-  //     url = '';https://issuu.com/encircletogether/docs/${name}
-  // }
-  const url = `https://issuu.com/encircletogether/docs/gsatoolkit/s/10192081`;
-  
-  
+  let url = '';
+
+  switch(name) {
+    case 'gsatoolkit':
+      url = 'https://issuu.com/encircletogether/docs/gsatoolkit/s/10192081';
+      break;
+    case 'encircle_magazine_draft__2':
+      url = '';
+      break;
+    case 'explanation_of_homosexuality':
+      url = '';
+      break;
+    case 'soce_12.9.17':
+      url = '';
+      break;
+    case 'gregprince_12.9.17':
+      url = '';
+      break;
+    case '_3_admirationoranimosity_12.9.17':
+      url = '';
+      break;
+    case 'contrast-series3_final':
+      url = '';
+      break;
+    case 'aparentsguide':
+      url = '';
+      break;
+    case 'whataboutgender':
+      url = '';
+      break;
+    case 'admiration_spa':
+      url = '';
+      break;
+    case 'sciencedogma_spa':
+      url = '';
+      break;
+    case 'sexualorientation_spa':
+      url = '';
+      break;
+    case 'explanation_spa':
+      url = '';
+      break;
+    case 'whataboutgender_spa':
+      url = '';
+      break;
+    default:
+      url = '';
+  }
+
   return(
     <WebView
     androidHardwareAccelerationDisabled
-    source={{uri: url}}
+    source={{ uri: url }}
     />
   )
 };
