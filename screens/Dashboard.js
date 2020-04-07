329ace6ff0c50947a0c7b2ebe2a257c25df9a008
neb <10528790@my.uvu.edu>
2020-04-06 21:48:51 -0600

    converting dashboard from class to function

---
+++ screens/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Dashboard = (props) => {
+  let events = [];
+  let resources = [];
+  
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
