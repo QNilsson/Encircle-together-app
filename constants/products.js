2350759f31b3fd17b9db1fb727c46caf8d3ae50a
neb <10528790@my.uvu.edu>
2020-04-23 21:48:17 -0600

    calendar styling, static shop content

---
+++ constants/products.js
@@ -0,0 +1,139 @@
+import Product from '../models/product';
+
+export const PRODUCTS = [
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/NoSidesOnlyLove-11_360x.jpg?v=1583945446',
+    'The T-Shirt',
+    '20.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/Screen_Shot_2020-01-22_at_1.01.21_PM_360x.png?v=1579724014',
+    'Proud of My Identity',
+    '10.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-a12ebc16_360x.jpg?v=1574264854',
+    'The Embroidered Polo',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-402fb1bc_360x.jpg?v=1574264693',
+    'The Embroidered Pocket Tee',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-0c412e41_360x.jpg?v=1574264445',
+    'The Long Sleeve (Champion)',
+    '35.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-26013d04_360x.jpg?v=1574224217',
+    'The Trucker Cap',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-45a62b15_360x.jpg?v=1574264353',
+    'The Beanie',
+    '20.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/Artboard_1iconic_360x.jpg?v=1578968890',
+    'Iconic.',
+    '15.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/0X7A1278_360x.jpg?v=1578968986',
+    'The Hoodie',
+    '30.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/NoSidesOnlyLove-96_360x.jpg?v=1578969324',
+    'The Tank',
+    '10.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/NoSidesOnlyLove-4_360x.jpg?v=1578968673',
+    'The T-Shirt (Dolman)',
+    '10.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-3d28cfa2_360x.jpg?v=1561411842',
+    'Encircle Embroidered Apron',
+    '30.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/Artboard1_360x.jpg?v=1586367748',
+    'Encircle Store Gift Card',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-bb77fc67_360x.jpg?v=1574221604',
+    'The Backpack',
+    '40.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-40727ecb_360x.jpg?v=1574222480',
+    'The Encircle Sticker',
+    '3.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-e1a74006_360x.jpg?v=1574216632',
+    'The iPhone Case',
+    '15.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-258333e4_360x.jpg?v=1574223519',
+    'The Laptop Sleeve',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-dd582149_360x.jpg?v=1574222143',
+    'The No Sides, Only Love Sticker',
+    '3.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-582b6195_360x.jpg?v=1574215834',
+    'The Phone Case (Glitter Edition)',
+    '15.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-e76945c0_360x.jpg?v=1574216290',
+    'The Phone Case (Samsung Edition)',
+    '15.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/mockup-bf097134_360x.jpg?v=1574223174',
+    'The Throw',
+    '40.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/DSC_8080_360x.jpg?v=1578968588',
+    'Stories from Encircle',
+    '30.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/pamphlets-ENG_Page_4_360x.jpg?v=1578968253',
+    'Admiration or Animosity (10 pack)',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/pamphlets-ENG_Page_3_360x.jpg?v=1578968217',
+    'An Explanation of Homosexuality (10 pack)',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/pamphlets-ENG_Page_2_360x.jpg?v=1578968125',
+    'Can A Person\'s Sexual Orientation Be Changed (10 pack)',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/ScienceDogma_ENG_Page_01_360x.jpg?v=1578967842',
+    'Science vs Dogma (10 pack)',
+    '25.00'
+  ),
+  new Product(
+    'https://cdn.shopify.com/s/files/1/0156/5841/8276/products/WhatAboutGender_Page_01_360x.jpg?v=1578967686',
+    'What About Gender (10-pack)',
+    '25.00'
+  )
+];
