fdfd38d5a9b6c19df41d261bdca3dc1e0a4c673c
VentusAlpha <roxasbeta@gmail.com>
2020-11-01 14:27:15 -0700

    DashboardWelcome.js updated styles

--- components/DashboardWelcome.js
+++ components/DashboardWelcome.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import * as Font from "expo-font";
 import { ActivityIndicator } from "react-native";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
 
 class DashboardWelcome extends Component {
   constructor(props) {
@@ -11,11 +11,11 @@ class DashboardWelcome extends Component {
 
   async componentWillMount() {
     await Font.loadAsync({
-      'ModernoFB': require('../assets/fonts/ModernoFB-Semibold.otf'),
-      'Futura-Light': require('../assets/fonts/Futura-Light.ttf'),
-      'Futura-Book': require('../assets/fonts/Futura-Book.ttf'),
-      'Futura-Medium': require('../assets/fonts/Futura-Medium.ttf'),
-      'Futura-Bold': require('../assets/fonts/Futura-Bold.ttf'),
+      ModernoFB: require("../assets/fonts/ModernoFB-Semibold.otf"),
+      "Futura-Light": require("../assets/fonts/Futura-Light.ttf"),
+      "Futura-Book": require("../assets/fonts/Futura-Book.ttf"),
+      "Futura-Medium": require("../assets/fonts/Futura-Medium.ttf"),
+      "Futura-Bold": require("../assets/fonts/Futura-Bold.ttf"),
     });
     this.setState({ loading: false });
   }
@@ -25,45 +25,33 @@ class DashboardWelcome extends Component {
       return <ActivityIndicator />;
     }
     return (
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>Welcome Back</Text>
-          <Text style={styles.subTitle}>Make today a great day.</Text>
-        </View>
+      <View style={styles.titleContainer}>
+        <ImageBackground
+          source={require("../assets/backgroundImage.png")}
+          style={styles.backgroundImage}
+        ></ImageBackground>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    titleContainer: {
-      backgroundColor: 'white',
-      width:'100%',
-      padding: 20,
-      borderBottomEndRadius: 30,
-      borderBottomStartRadius: 30,
-      marginBottom: 20,
-  
-      paddingLeft: 12,
-      paddingRight: 12,
-      top: 0,
-      // height: '100%',
-     
-    },
-    title: {
-      fontSize: 40,
-      textAlign: 'center',
-      color: '#2B2B2B',
-      marginTop: '20%',
-      fontFamily: 'ModernoFB',
-    },
-    subTitle: {
-      fontSize: 18,
-      color: '#686868',
-      textAlign: 'center',
-      margin: 8,
-      marginBottom: 20,
-      alignItems: 'center',
-      fontFamily: 'Futura-Book'
-    },
+  titleContainer: {
+    width: "100%",
+    top: 0,
+    // height: '100%',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: 414,
+    height: 167,
+  },
+  image: {
+    resizeMode: "contain",
+    height: 100,
+  },
 });
 
 export default DashboardWelcome;
