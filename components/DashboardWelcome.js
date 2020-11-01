4d327ba69334236ea133212fd2364b8edea1c407
VentusAlpha <roxasbeta@gmail.com>
2020-11-01 14:34:33 -0700

    fonts add in comments

--- components/DashboardWelcome.js
+++ components/DashboardWelcome.js
@@ -16,6 +16,19 @@ class DashboardWelcome extends Component {
       "Futura-Book": require("../assets/fonts/Futura-Book.ttf"),
       "Futura-Medium": require("../assets/fonts/Futura-Medium.ttf"),
       "Futura-Bold": require("../assets/fonts/Futura-Bold.ttf"),
+      // "Claredon-Pro-Bold-Italic": require("./assets/fonts/Clarendon Text Pro Bold Italic.otf"),
+      // "Claredon-Pro-Italic": require("./assets/fonts/Clarendon Text Pro Italic.otf"),
+      // "Claredon-Pro-Reg": require("./assets/fonts/Clarendon Text Pro Reg.otf"),
+      // "Claredon-Pro-Bold": require("./assets/fonts/Clarendon Text Pro Bold.otf"),
+      // "DIN-Condensed-Reg": require("./assets/fonts/DIN Condensed Reg.otf"),
+      // "Garamond-Pro-Bold-Italic": require("./assets/fonts/Garamond Premier Pro Bold Italic.otf"),
+      // "Garamond-Pro-Bold": require("./assets/fonts/Garamond Premier Pro Bold.otf"),
+      // "Garamond-Pro-Italic": require("./assets/fonts/Garamond Premier Pro Italic.otf"),
+      // "Garamond-Pro-Medium-Italic": require("./assets/fonts/Garamond Premier Pro Medium Italic.otf"),
+      // "Garamond-Pro-Medium": require("./assets/fonts/Garamond Premier Pro Medium.otf"),
+      // "Garamond-Pro-Reg": require("./assets/fonts/Garamond Premier Pro Reg.otf"),
+      // "Garamond-Pro-Semibold-Italic": require("./assets/fonts/Garamond Premier Pro Semibold Italic.otf"),
+      // "Garamond-Pro-Semibold": require("./assets/fonts/Garamond Premier Pro Semibold.otf"),
     });
     this.setState({ loading: false });
   }
