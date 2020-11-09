59badba38e4ecb4a3f23d47a69e8b7378819ed98
seajensen <seajensen@gmail.com>
2020-11-09 07:43:56 -0700

    updated calendar and resource screens

--- App.js
+++ App.js
@@ -22,11 +22,21 @@ const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 // loads app with custom font
 const componentDidMount = () => {
   Font.loadAsync({
-    ModernoFB: require("./assets/fonts/ModernoFB-Semibold.otf"),
-    "Futura-Light": require("./assets/fonts/Futura-Light.ttf"),
-    "Futura-Book": require("./assets/fonts/Futura-Book.ttf"),
-    "Futura-Medium": require("./assets/fonts/Futura-Medium.ttf"),
-    "Futura-Bold": require("./assets/fonts/Futura-Bold.ttf"),
+    'ModernoFB': require('./assets/fonts/ModernoFB-Semibold.otf'),
+    'Futura-Light': require('./assets/fonts/Futura-Light.ttf'),
+    'Futura-Book': require('./assets/fonts/Futura-Book.ttf'),
+    'Futura-Medium': require('./assets/fonts/Futura-Medium.ttf'),
+    'Futura-Bold': require('./assets/fonts/Futura-Bold.ttf'),
+    'Clarendon': require('./assets/fonts/clarendon.otf'),
+    'Clarendon-Italic': require('./assets/fonts/clarendon-italic.otf'),
+    'Clarendon-Bold': require('./assets/fonts/clarendon-bold.otf'),
+    'Clarendon-Bold-Italic': require('./assets/fonts/clarendon-bold-italic.otf'),
+    'Din': require('./assets/fonts/din.otf'),
+    'Din-Bold': require('./assets/fonts/din-bold.otf'),
+    'Garamond': require('./assets/fonts/garamond.otf'),
+    'Garamond-Bold': require('./assets/fonts/garamond-bold.otf'),
+    'Garamond-Italic': require('./assets/fonts/garamond-italic.otf'),
+    'Garamond-Bold-Italic': require('./assets/fonts/garamond-bold-italic.otf'),
   });
 };
 
