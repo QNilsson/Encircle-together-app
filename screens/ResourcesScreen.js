027bf2cc653060a8313b9a91d6533d6a6424f290
VentusAlpha <roxasbeta@gmail.com>
2021-04-01 20:59:35 -0600

    initial and final branch commit

--- screens/ResourcesScreen.js
+++ screens/ResourcesScreen.js
@@ -1,17 +1,109 @@
 import React, { Component } from "react";
-import { Dimensions, StyleSheet, Text, View, FlatList, TouchableOpacity, Image, ImageBackground, SafeAreaView, ScrollView } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  Linking
+} from "react-native";
 
 // imports publication model used to load publicationData array
-import Publication from '../models/publication';
+import Publication from "../models/publication";
 
 export default class ResourcesScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      publications: []
+      publications: [],
     };
   }
+
+  outsideLink = (name) => {
+    let url = "";
+
+    // points webview uri to issuu publication story url (https://issuu.com/) - cannot pull this url using api (https://developer.issuu.com/)
+    switch (name) {
+      case "gsatoolkit":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/gsatoolkit/s/10192081"
+        );
+        break;
+      case "encircle_magazine_draft__2":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/encircle_magazine_draft__2/s/10297797"
+        );
+        break;
+      case "explanation_of_homosexuality":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/explanation_of_homosexuality/s/10297790"
+        );
+        break;
+      case "soce_12.9.17":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/soce_12.9.17/s/10297478"
+        );
+        break;
+      case "gregprince_12.9.17":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/gregprince_12.9.17/s/10283847"
+        );
+        break;
+      case "_3_admirationoranimosity_12.9.17":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/_3_admirationoranimosity_12.9.17/s/10224048"
+        );
+        break;
+      case "contrast-series3_final":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/contrast-series3_final/s/10297880"
+        );
+        break;
+      case "aparentsguide":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/aparentsguide/s/10224030"
+        );
+        break;
+      case "whataboutgender":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/whataboutgender/s/10224013"
+        );
+        break;
+      case "admiration_spa":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/admiration_spa/s/10297809"
+        );
+        break;
+      case "sciencedogma_spa":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/sciencedogma_spa/s/10297803"
+        );
+        break;
+      case "sexualorientation_spa":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/sexualorientation_spa/s/10297802"
+        );
+        break;
+      case "explanation_spa":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/explanation_spa/s/10297801"
+        );
+        break;
+      case "whataboutgender_spa":
+        Linking.openURL(
+          "https://issuu.com/encircletogether/docs/whataboutgender_spa/s/10297799"
+        );
+        break;
+      default:
+        url = "";
+    }
+  };
 
   imageHeight = "100%";
   imageWidth = "90%";
@@ -25,8 +117,8 @@ export default class ResourcesScreen extends Component {
     const url = `http://api.issuu.com/1_0?action=issuu.documents.list&apiKey=bmcyheq8ih6qlsr0ktxgzsfppzkjruw2&format=json&pageSize=30&signature=ce7ab9c425b8c58767948e25cd5d5a92`;
 
     fetch(url)
-      .then(res => res.json())
-      .then(resData => {
+      .then((res) => res.json())
+      .then((resData) => {
         // stores unpackaged response data
         const publications = resData.rsp._content.result._content;
         // stores publication objects
@@ -52,16 +144,16 @@ export default class ResourcesScreen extends Component {
         }
 
         this.setState({
-          publications: publicationData
+          publications: publicationData,
         });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
 
   // navigates to resource screen - sends resource name as parameter
   selectedResource(name) {
     this.props.navigation.navigate("ResourceScreen", {
-      resourceName: name
+      resourceName: name,
     });
   }
 
@@ -75,7 +167,7 @@ export default class ResourcesScreen extends Component {
               numColumns={2}
               style={styles.flatlist}
               data={this.state.publications}
-              keyExtractor={publication => publication.docId}
+              keyExtractor={(publication) => publication.docId}
               ListHeaderComponent={
                 <View>
                   <ImageBackground
@@ -88,23 +180,19 @@ export default class ResourcesScreen extends Component {
                 <View style={styles.item} key={item.docId}>
                   <TouchableOpacity
                     onPress={() =>
-                      this.props.navigation.navigate("Resource", {
-                        resourceName: item.name
-                      })
+                      this.outsideLink(item.name)
                     }
                   >
                     {
                       <Image
                         style={{
                           height: 300,
-                          width: '99%',
-                          alignItems: "center"
-                      
+                          width: "99%",
+                          alignItems: "center",
                         }}
-
                         // points to resource cover image (see issuu api docs)
                         source={{
-                          uri: `https://image.issuu.com/${item.docId}/jpg/page_1_thumb_large.jpg`
+                          uri: `https://image.issuu.com/${item.docId}/jpg/page_1_thumb_large.jpg`,
                         }}
                       />
                     }
@@ -121,21 +209,21 @@ export default class ResourcesScreen extends Component {
   }
 }
 
-let screenWidth = Dimensions.get('window').width
+let screenWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: '20%',
+    height: "20%",
     backgroundColor: "#fff",
   },
   resources: {
     fontSize: 34,
-    marginBottom: '20%',
-    marginTop: '25%',
+    marginBottom: "20%",
+    marginTop: "25%",
     textAlign: "center",
     color: "#fff",
-    fontFamily: 'Clarendon-Regular',
+    fontFamily: "Clarendon-Regular",
   },
   backgroundImage: {
     resizeMode: "contain",
@@ -149,25 +237,24 @@ const styles = StyleSheet.create({
     margin: 8,
     marginBottom: 10,
     alignItems: "center",
-    fontFamily: 'Garamond-Regular'
+    fontFamily: "Garamond-Regular",
   },
   item: {
     flex: 1,
     textAlign: "center",
     alignContent: "center",
     marginBottom: 10,
-    marginTop: 10
-
+    marginTop: 10,
   },
   titleText: {
     fontSize: 16,
-    width: '98%',
+    width: "98%",
     fontWeight: "500",
     color: "#2B2B2B",
     textAlign: "center",
-    fontFamily: 'Garamond-Regular',
+    fontFamily: "Garamond-Regular",
     marginBottom: 0,
     margin: 0,
-    paddingTop: 5
-  }
+    paddingTop: 5,
+  },
 });
